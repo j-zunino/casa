75f0c97f51efaa781f54afa003063cb72bf33f48
j-zunino <zuninojuanbautista@gmail.com>
2026-02-12 16:01:42 -0300

    feat(client): add TanStack Router

---
+++ client/src/routes/__root.tsx
@@ -0,0 +1,21 @@
+import { Link, Outlet, createRootRoute } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+
+const RootLayout = () => {
+    return (
+        <>
+            <div className="p-2 flex gap-2">
+                <Link to="/" className="[&.active]:font-bold">
+                    Home
+                </Link>
+            </div>
+            <hr />
+
+            <Outlet />
+
+            <TanStackRouterDevtools />
+        </>
+    );
+};
+
+export const Route = createRootRoute({ component: RootLayout });
