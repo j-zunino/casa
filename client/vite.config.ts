1f89dee578a719c1e9f4f5d48cdb495b2b19d9ca
j-zunino <zuninojuanbautista@gmail.com>
2026-02-13 21:51:44 -0300

    feat(client): add Tailwindcss

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 import tanstackRouter from '@tanstack/router-plugin/vite';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [
@@ -9,5 +10,6 @@ export default defineConfig({
             autoCodeSplitting: true,
         }),
         react(),
+        tailwindcss(),
     ],
 });
