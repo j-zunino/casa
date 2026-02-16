b476f96f465a23c6d5105218af9221ffc843e23b
j-zunino <zuninojuanbautista@gmail.com>
2026-02-16 20:02:14 -0300

    feat: add unified eslint

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+
+export default [
+    {
+        ignores: ['**/dist', '**/node_modules'],
+    },
+
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+
+    {
+        files: ['client/**/*.{ts,tsx}'],
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            ...reactRefresh.configs.vite.rules,
+        },
+        languageOptions: {
+            globals: globals.browser,
+        },
+    },
+
+    {
+        files: ['server/**/*.ts'],
+        languageOptions: {
+            globals: globals.node,
+        },
+    },
+];
