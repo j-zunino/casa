5614cde88126b1ac5bb129edaa21b2b5eb3151bd
j-zunino <zuninojuanbautista@gmail.com>
2026-02-19 18:02:09 -0300

    feat(server): init prisma

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 process.loadEnvFile();
 const envSchema = z.object({
     PORT: z.coerce.number().optional().default(3000),
-    DB_URL: z.url(),
+    DATABASE_URL: z.url(),
     CORS_WHITELIST: z
         .string()
         .transform((urls) => urls.split(',').map((url) => url.trim()))
