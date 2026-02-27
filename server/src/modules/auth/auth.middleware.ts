aa2ec88ee3c752bb67549e7624157a830993bc22
j-zunino <zuninojuanbautista@gmail.com>
2026-02-27 17:06:08 -0300

    feat(server): add auth middleware

---
+++ server/src/modules/auth/auth.middleware.ts
@@ -0,0 +1,32 @@
+import { ErrorCodes } from '@casa/shared';
+import { type NextFunction, type Request, type Response } from 'express';
+import { AppError } from '../../utils/index.ts';
+import { auth } from './auth.ts';
+
+export const requireAuth = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    const session = await auth.api.getSession({
+        headers: {
+            cookie: req.headers.cookie,
+            authorization: req.headers.authorization,
+        },
+    });
+
+    if (!session) {
+        return next(
+            new AppError(
+                'Not authenticated',
+                401,
+                ErrorCodes.NOT_AUTHENTICATED,
+            ),
+        );
+    }
+
+    res.locals.user = session.user;
+    res.locals.session = session.session;
+
+    next();
+};
--- server/src/modules/auth/index.ts
+++ server/src/modules/auth/index.ts
@@ -1,2 +1,3 @@
 export { auth } from './auth.ts';
 export { router as authRouter } from './auth.routes.ts';
+export { requireAuth } from './auth.middleware.ts';
