cfab396c19f6bc3cf97240e829082b8861a4e5df
j-zunino <zuninojuanbautista@gmail.com>
2026-02-28 16:18:13 -0300

    feat(server): replace tsc with tsup

---
+++ server/tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  platform: 'node',
+  format: 'esm',
+  outDir: 'dist',
+  dts: true,
+  splitting: false,
+  sourcemap: true,
+  external: ['@casa/shared'],
+});
