547c519e82fdd60a1ede42862227d7ab0993dfe2
j-zunino <zuninojuanbautista@gmail.com>
2026-03-02 17:54:44 -0300

    feat(flake): use direnv

--- flake.nix
+++ flake.nix
@@ -1,20 +1,27 @@
 {
-  description = "Monorepo dev env";
-
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
   outputs = {
     self,
     nixpkgs,
+    ...
   }: let
     system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
+    pkgs = nixpkgs.legacyPackages.${system};
   in {
     devShells.${system}.default = pkgs.mkShell {
       packages = with pkgs; [
+        emmet-ls
+        eslint_d
+        nodejs_24
+        pnpm
         postgresql_16
+        prettierd
+        prisma-language-server
+        tailwindcss-language-server
+        typescript-language-server
+        vscode-langservers-extracted
+        vtsls
       ];
 
       buildInputs = with pkgs; [
@@ -23,16 +30,13 @@
         prisma
       ];
 
-      PGPORT = 5432;
-      PGUSER = "casa";
-      PGPASSWORD = "casa";
+      pkg_config_path = "${pkgs.openssl.dev}/lib/pkgconfig";
+      prisma_schema_engine_binary = "${pkgs.prisma-engines}/bin/schema-engine";
+      prisma_query_engine_binary = "${pkgs.prisma-engines}/bin/query-engine";
+      prisma_query_engine_library = "${pkgs.prisma-engines}/lib/libquery_engine.node";
+      prisma_fmt_binary = "${pkgs.prisma-engines}/bin/prisma-fmt";
 
       shellHook = ''
-        export PKG_CONFIG_PATH=${pkgs.openssl.dev}/lib/pkgconfig
-        export PRISMA_SCHEMA_ENGINE_BINARY=${pkgs.prisma-engines}/bin/schema-engine
-        export PRISMA_QUERY_ENGINE_BINARY=${pkgs.prisma-engines}/bin/query-engine
-        export PRISMA_QUERY_ENGINE_LIBRARY=${pkgs.prisma-engines}/lib/libquery_engine.node
-        export PRISMA_FMT_BINARY=${pkgs.prisma-engines}/bin/prisma-fmt
         export PGDATA=$PWD/server/.pgdata
         export PGHOST=$PWD/server/.pgsocket
 
@@ -45,11 +49,11 @@
         fi
 
         if ! pg_ctl -D "$PGDATA" status > /dev/null 2>&1; then
-              echo "Starting PostgreSQL..."
-              pg_ctl -D "$PGDATA" \
-                  -l "$PGDATA/logfile" \
-                  -o "-k $PGHOST" \
-                  start
+            echo "Starting PostgreSQL..."
+            pg_ctl -D "$PGDATA" \
+            -l "$PGDATA/logfile" \
+            -o "-k $PGHOST" \
+            start
         fi
 
         until pg_isready -h "$PGHOST" -p "$PGPORT" > /dev/null 2>&1; do
@@ -61,19 +65,16 @@
 
         # Create app role if missing
         psql -d postgres -tc "SELECT 1 FROM pg_roles WHERE rolname='casa'" | grep -q 1 || \
-            psql -d postgres -c "CREATE ROLE casa WITH LOGIN PASSWORD 'casa' CREATEDB;"
+        psql -d postgres -c "CREATE ROLE casa WITH LOGIN PASSWORD 'casa' CREATEDB;"
 
         # Create DB if missing
         psql -d postgres -tc "SELECT 1 FROM pg_database WHERE datname='casa_dev'" | grep -q 1 || \
-            psql -d postgres -c "CREATE DATABASE casa_dev OWNER casa;"
+        psql -d postgres -c "CREATE DATABASE casa_dev OWNER casa;"
 
         # Now switch to app user
         export PGUSER=casa
 
-        echo "PostgreSQL ready"
-        echo "postgresql://$PGUSER:$PGPASSWORD@localhost:$PGPORT/casa_dev?host=$PGHOST"
-
-        trap "pg_ctl -D \"$PGDATA\" stop" EXIT
+        echo "PostgreSQL ready at: postgresql://$PGUSER:$PGPASSWORD@localhost:$PGPORT/casa_dev?host=$PGHOST"
       '';
     };
   };
