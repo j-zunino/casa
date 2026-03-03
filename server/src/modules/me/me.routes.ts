1c6054393e39089b58163b6ba55c5055864ff45c
j-zunino <zuninojuanbautista@gmail.com>
2026-03-03 03:18:14 -0300

    fix(server): remove try-catch from me route

--- server/src/modules/me/me.routes.ts
+++ server/src/modules/me/me.routes.ts
@@ -5,16 +5,12 @@ import { requireAuth } from '../auth/index';
 export const router: Router = Router();
 
 router.get('/', requireAuth, async (_req: Request, res: Response, next) => {
-    try {
-        const { user } = res.locals;
+    const { user } = res.locals;
 
-        const response: ApiResponse<typeof user> = {
-            success: true,
-            data: user,
-        };
+    const response: ApiResponse<typeof user> = {
+        success: true,
+        data: user,
+    };
 
-        res.json(response);
-    } catch (error) {
-        next(error);
-    }
+    res.json(response);
 });
