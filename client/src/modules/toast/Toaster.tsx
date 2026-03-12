29fb25e224b90e9dd9f0bc1b8cde69bd8f77a84c
j-zunino <zuninojuanbautista@gmail.com>
2026-03-12 18:37:02 -0300

    fix(client): prevent dismissing loading toasts

--- client/src/modules/toast/Toaster.tsx
+++ client/src/modules/toast/Toaster.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import toast, { subscribe, type ToastConfig } from '.';
 import { Toast } from '../../components/ui';
+import type { ToastVariant } from './toast.config.ts';
 
 export const Toaster = () => {
     const [toasts, setToasts] = useState<ToastConfig[]>([]);
@@ -10,6 +11,14 @@ export const Toaster = () => {
         return unsubscribe;
     }, []);
 
+    const handleDissmis = (id: ToastConfig['id'], variant: ToastVariant) => {
+        if (variant === 'loading') {
+            return;
+        }
+
+        toast.dismiss(id);
+    };
+
     return (
         <div className="fixed right-2 bottom-2 flex flex-col items-end gap-2">
             {toasts.map((t) => (
@@ -18,7 +27,7 @@ export const Toaster = () => {
                     id={t.id}
                     variant={t.variant}
                     message={t.message}
-                    onDismiss={() => toast.dismiss(t.id)}
+                    onDismiss={() => handleDissmis(t.id, t.variant)}
                 />
             ))}
         </div>
