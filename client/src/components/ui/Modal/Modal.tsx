1b0c2bea2298320a9593f6f0a9bac09803d81d99
j-zunino <zuninojuanbautista@gmail.com>
2026-03-30 16:12:30 -0300

    feat(client): improve Modal in mobile

--- client/src/components/ui/Modal/Modal.tsx
+++ client/src/components/ui/Modal/Modal.tsx
@@ -15,13 +15,17 @@ export const Modal = forwardRef<HTMLDialogElement, Props>(
                 ref={ref}
                 onClose={onClose}
                 onClick={onClose}
-                className="fixed top-1/2 left-1/2 m-0 -translate-x-1/2 -translate-y-1/2 backdrop:bg-black/20 backdrop:backdrop-blur-[2px]"
+                className="fixed top-1/2 left-1/2 m-0 w-full max-w-md -translate-x-1/2 -translate-y-1/2 animate-slide-in bg-secondary-2 text-secondary-12 backdrop:bg-black/20 backdrop:backdrop-blur-[2px]"
             >
                 <div
                     onClick={(e) => e.stopPropagation()}
-                    className="min-w-md space-y-8 bg-secondary-2 p-4 text-secondary-12"
+                    className="space-y-8 p-4"
                 >
-                    <button className="fixed top-4 right-4" onClick={onClose}>
+                    {/* TODO: Replace with icon */}
+                    <button
+                        className="fixed top-4 right-4 cursor-pointer px-2 hover:bg-secondary-4"
+                        onClick={onClose}
+                    >
                         x
                     </button>
 
