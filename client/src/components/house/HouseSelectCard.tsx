438441951e5a7471784411927bc324ad29dc53ee
j-zunino <zuninojuanbautista@gmail.com>
2026-03-30 15:17:45 -0300

    feat(client): make HouseSelectCard a button

--- client/src/components/house/HouseSelectCard.tsx
+++ client/src/components/house/HouseSelectCard.tsx
@@ -6,12 +6,12 @@ interface Props {
 // TODO: Use House Logo
 export const HouseSelectCard = ({ name, onClick }: Props) => {
     return (
-        <div className="group max-w-40 cursor-pointer active:scale-(--scale-active)">
-            <button
-                onClick={onClick}
-                className="h-40 w-40 bg-secondary-6 outline-(--outline-color) group-hover:outline-2"
-            />
+        <button
+            onClick={onClick}
+            className="group max-w-40 cursor-pointer outline-0 active:scale-(--scale-active)"
+        >
+            <div className="h-40 w-40 bg-secondary-6 outline-(--outline-color) group-hover:outline-2 group-focus-visible:outline-2" />
             <h3 className="w-full text-center wrap-break-word">{name}</h3>
-        </div>
+        </button>
     );
 };
