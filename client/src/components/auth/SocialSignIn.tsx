5af56ba764fa24576d7a973ef9e4896ade378964
j-zunino <zuninojuanbautista@gmail.com>
2026-04-02 17:28:33 -0300

    feat(client): update SignUp form

--- client/src/components/auth/SocialSignIn.tsx
+++ client/src/components/auth/SocialSignIn.tsx
@@ -1,18 +1,12 @@
+import { GithubLogoIcon } from '@phosphor-icons/react';
 import { handleGithubSingIn } from '../../modules/auth';
-import { Button } from '../ui';
+import { Button } from '../ui/button';
 
 export const SocialSignIn = () => {
     return (
-        <div className="w-full flex flex-col gap-2">
-            <div className="flex items-center gap-2">
-                <span className="h-px w-full bg-secondary-6" />
-                <span className="text-secondary-6 text-sm">OR</span>
-                <span className="h-px w-full bg-secondary-6" />
-            </div>
-
-            <Button onClick={handleGithubSingIn} variant="outline">
-                Sign In with GitHub
-            </Button>
-        </div>
+        <Button onClick={handleGithubSingIn} variant="outline">
+            <GithubLogoIcon weight="bold" />
+            Sign In with GitHub
+        </Button>
     );
 };
--- client/src/components/layouts/AuthLayout.tsx
+++ client/src/components/layouts/AuthLayout.tsx
@@ -1,22 +1,13 @@
 import type { ReactNode } from 'react';
 
 interface Props {
-    title: string;
-    description: string;
     children: ReactNode;
 }
 
-export const AuthLayout = ({ title, description, children }: Props) => {
+export const AuthLayout = ({ children }: Props) => {
     return (
         <div className="flex flex-col items-center">
-            <div className="w-full max-w-md space-y-8 p-4">
-                <div>
-                    <h1 className="text-2xl font-bold">{title}</h1>
-                    <p className="text-secondary-11">{description}</p>
-                </div>
-
-                <div className="flex flex-col gap-2">{children}</div>
-            </div>
+            <div className="w-full max-w-md p-4">{children}</div>
         </div>
     );
 };
