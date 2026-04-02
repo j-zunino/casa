b26ea3d7bd6601192bac78df98155e0c623721f3
j-zunino <zuninojuanbautista@gmail.com>
2026-04-02 20:40:44 -0300

    feat(client): update CreateHouseForm

--- client/src/routes/_authenticated/index.tsx
+++ client/src/routes/_authenticated/index.tsx
@@ -1,13 +1,17 @@
+import { Button } from '@/components/ui/button';
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from '@/components/ui/dialog';
 import { createFileRoute } from '@tanstack/react-router';
 import { CreateHouseForm, HouseSelectCard } from '../../components/house';
-import { Button } from '../../components/ui';
-import { Modal } from '../../components/ui/Modal';
 import { authClient, setActiveHouse } from '../../modules/auth';
-import { useModal } from '../../modules/modal';
 
 const Index = () => {
-    const createHouseModal = useModal();
-
     // TODO: Move to context
     const { data: orgs } = authClient.useListOrganizations();
 
@@ -22,24 +26,25 @@ const Index = () => {
                     />
                 ))}
 
-                <HouseSelectCard
-                    name="Add one"
-                    onClick={createHouseModal.open}
-                />
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <HouseSelectCard name="Add one" />
+                    </DialogTrigger>
+                    <DialogContent>
+                        <DialogHeader>
+                            <DialogTitle>Create House</DialogTitle>
+                            <DialogDescription>
+                                This will be your shared space
+                            </DialogDescription>
+                        </DialogHeader>
+                        <CreateHouseForm />
+                    </DialogContent>
+                </Dialog>
             </div>
 
             <Button onClick={() => {}} variant="outline">
                 Manage House
             </Button>
-
-            <Modal
-                ref={createHouseModal.ref}
-                onClose={createHouseModal.close}
-                title="Create House"
-                description="This will be your shared space"
-            >
-                <CreateHouseForm />
-            </Modal>
         </div>
     );
 };
