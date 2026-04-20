0d5ab5c41651a80c169d0411f61c5a7a3027fd12
j-zunino <zuninojuanbautista@gmail.com>
2026-04-19 21:10:37 -0300

    feat(client): add house settings page layout

--- client/src/routes/_authenticated/account/houses/$slug.tsx
+++ client/src/routes/_authenticated/account/houses/$slug.tsx
@@ -1,9 +1,74 @@
-import { createFileRoute, Link } from '@tanstack/react-router';
+import {
+    HouseAvatar,
+    HouseAvatarFallback,
+    HouseAvatarImage,
+    HouseLabel,
+} from '@/components/house';
+import { Button } from '@/components/ui/button';
+import {
+    CaretLeftIcon,
+    CaretRightIcon,
+    HouseLineIcon,
+    TrashIcon,
+    UserIcon,
+} from '@phosphor-icons/react';
+import { createFileRoute, Link, useRouteContext } from '@tanstack/react-router';
 
 const RouteComponent = () => {
+    const { house } = useRouteContext({ from: '__root__' });
+
     return (
-        <div>
-            <Link to="/">Back</Link>
+        <div className="flex grow flex-col items-center p-8">
+            <div className="flex w-full max-w-xl flex-col items-center justify-start gap-8">
+                <div className="w-full justify-start">
+                    <Button variant="ghost">
+                        <Link to=".." className="flex items-center">
+                            <CaretLeftIcon />
+                            Back
+                        </Link>
+                    </Button>
+                </div>
+
+                <div className="w-30 rounded-md transition outline-none select-none">
+                    <HouseAvatar>
+                        <HouseAvatarImage
+                            src={house.active?.logo ?? undefined}
+                            alt={house.active?.name}
+                        />
+
+                        <HouseAvatarFallback>
+                            <HouseLineIcon />
+                        </HouseAvatarFallback>
+                    </HouseAvatar>
+
+                    <HouseLabel label={house.active?.name} />
+                </div>
+
+                <div className="flex w-full flex-col gap-2">
+                    <Button
+                        asChild
+                        size="lg"
+                        variant="outline"
+                        className="justify-between"
+                    >
+                        <Link to=".">
+                            <span className="flex items-center gap-2">
+                                <UserIcon />
+                                Members
+                            </span>
+
+                            <CaretRightIcon />
+                        </Link>
+                    </Button>
+
+                    <Button asChild size="lg" variant="destructive">
+                        <Link to=".">
+                            <TrashIcon />
+                            Delete house
+                        </Link>
+                    </Button>
+                </div>
+            </div>
         </div>
     );
 };
@@ -12,4 +77,3 @@ export const Route = createFileRoute('/_authenticated/account/houses/$slug')({
     staticData: { homePath: '/h/$slug' },
     component: RouteComponent,
 });
-
