836a0d4a1f5127f7e6cb9c48d9fdcb1856319f62
j-zunino <zuninojuanbautista@gmail.com>
2026-07-03 22:09:34 -0300

    feat(client): add shimmer to Loading

--- client/src/components/common/Loading.tsx
+++ client/src/components/common/Loading.tsx
@@ -2,11 +2,9 @@ import { Spinner } from "@/components/ui/spinner";
 
 export const Loading = () => {
     return (
-        <div className="flex h-screen items-center justify-center text-sm select-none">
-            <span className="flex animate-pulse items-center gap-1">
-                <Spinner />
-                Loading...
-            </span>
+        <div className="flex h-screen shimmer items-center justify-center gap-1 text-sm select-none">
+            <Spinner />
+            Loading...
         </div>
     );
 };
