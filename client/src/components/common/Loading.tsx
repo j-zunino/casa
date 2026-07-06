23b1d40cfa572ae1ab3bcac6d0f44b58c12f5e6a
j-zunino <zuninojuanbautista@gmail.com>
2026-07-06 16:55:12 -0300

    fix(client): bump shadcn version to 4.13

--- client/src/components/common/Loading.tsx
+++ client/src/components/common/Loading.tsx
@@ -2,7 +2,7 @@ import { Spinner } from "@/components/ui/spinner";
 
 export const Loading = () => {
     return (
-        <div className="shimmer flex h-full w-full grow items-center justify-center gap-1 text-sm select-none">
+        <div className="flex h-full w-full grow shimmer items-center justify-center gap-1 text-sm select-none">
             <Spinner />
             Loading...
         </div>
