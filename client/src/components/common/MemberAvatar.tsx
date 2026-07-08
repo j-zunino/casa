f71fb820fecc37467a6ab61f879a8f5bd2d8f1ac
j-zunino <zuninojuanbautista@gmail.com>
2026-07-07 22:58:14 -0300

    refactor(client): split MemberAvatar into Profile and ProfileAvatar

--- client/src/components/common/MemberAvatar.tsx
+++ client/src/components/common/MemberAvatar.tsx
@@ -1,35 +1,32 @@
-import {
-    Avatar,
-    AvatarFallback,
-    AvatarImage,
-    AvatarLabel,
-} from "@/components/ui/avatar";
+// TODO: Rename file
+import { cn } from "@/lib/utils";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { UserIcon } from "@phosphor-icons/react";
 
-import type { User } from "@/features/auth/types";
-import type { Member } from "better-auth/client/plugins";
-import { RoleBadge } from "./RoleBadge";
+import type { ComponentProps } from "react";
 
-interface Props {
-    name: User["name"];
-    image: User["image"];
-    role?: Member["role"];
-}
-
-export const MemberAvatar = ({ name, image, role }: Props) => {
+export const Profile = ({ className, ...props }: ComponentProps<"div">) => {
     return (
-        <div className="flex items-center gap-1.5">
-            <Avatar size="sm">
-                <AvatarImage src={image ?? undefined} alt={name} />
-                <AvatarFallback>
-                    <UserIcon />
-                </AvatarFallback>
-            </Avatar>
+        <div
+            className={cn("flex items-center gap-1.5", className)}
+            {...props}
+        />
+    );
+};
 
-            <div className="flex flex-wrap gap-x-1.5">
-                <AvatarLabel>{name}</AvatarLabel>
-                {role && <RoleBadge role={role} />}
-            </div>
-        </div>
+export const ProfileAvatar = ({
+    size = "sm",
+    src,
+    alt,
+    ...props
+}: ComponentProps<typeof Avatar> & ComponentProps<typeof AvatarImage>) => {
+    return (
+        <Avatar size={size} {...props}>
+            <AvatarImage src={src ?? undefined} alt={alt} />
+            <AvatarFallback>
+                <UserIcon />
+            </AvatarFallback>
+        </Avatar>
     );
 };
