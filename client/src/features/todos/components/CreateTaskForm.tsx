1a337528789fd42dec0114fb9a39d1f9399b186b
j-zunino <zuninojuanbautista@gmail.com>
2026-08-02 17:31:20 -0300

    refactor(client): move CreateTaskForm to own component

---
+++ client/src/features/todos/components/CreateTaskForm.tsx
@@ -0,0 +1,149 @@
+import { format } from "date-fns";
+import { useState } from "react";
+
+import { Required } from "@/components/common/Required";
+import { Button } from "@/components/ui/button";
+import { ButtonGroup } from "@/components/ui/button-group";
+import { Calendar } from "@/components/ui/calendar";
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+import {
+    CalendarIcon,
+    CheckCircleIcon,
+    GlobeIcon,
+    LockIcon,
+    PlusIcon,
+    XIcon,
+} from "@phosphor-icons/react";
+
+const visibilityValues = [
+    { label: "Private", icon: <LockIcon />, value: "PRIVATE" as const },
+    { label: "Public", icon: <GlobeIcon />, value: "PUBLIC" as const },
+];
+
+export const CreateTaskForm = () => {
+    const [createSub, setCreateSub] = useState(false);
+    const [date, setDate] = useState<Date>();
+
+    return (
+        <form>
+            <FieldGroup>
+                <Field>
+                    <FieldLabel htmlFor="title">
+                        Title <Required />
+                    </FieldLabel>
+                    <Input id="name" placeholder="Grocery's list" />
+                </Field>
+
+                <Field>
+                    <FieldLabel>Sub tasks</FieldLabel>
+                    {createSub ? (
+                        <ButtonGroup>
+                            <Input size="sm" placeholder="Type something..." />
+                            <Button
+                                type="button"
+                                size="sm"
+                                variant="outline"
+                                onClick={() => setCreateSub(false)}
+                            >
+                                <PlusIcon /> Add
+                            </Button>
+                            <Button
+                                type="button"
+                                size="icon-sm"
+                                variant="ghost"
+                                className="ml-1.5"
+                                onClick={() => setCreateSub(false)}
+                            >
+                                <XIcon />
+                            </Button>
+                        </ButtonGroup>
+                    ) : (
+                        <Button
+                            type="button"
+                            size="sm"
+                            variant="outline"
+                            onClick={() => setCreateSub(true)}
+                        >
+                            <CheckCircleIcon />
+                            Add new sub task
+                        </Button>
+                    )}
+                </Field>
+
+                <Field orientation="horizontal">
+                    <Field>
+                        <FieldLabel htmlFor="visibility">
+                            Visibility <Required />
+                        </FieldLabel>
+                        <Select defaultValue="PRIVATE" name="visibility">
+                            <SelectTrigger>
+                                <SelectValue />
+                            </SelectTrigger>
+
+                            <SelectContent>
+                                <SelectGroup>
+                                    {visibilityValues.map((item) => (
+                                        <SelectItem
+                                            key={item.value}
+                                            value={item.value}
+                                        >
+                                            {item.icon}
+                                            {item.label}
+                                        </SelectItem>
+                                    ))}
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+                    </Field>
+
+                    <Field>
+                        <FieldLabel htmlFor="due-date">Due date</FieldLabel>
+                        <Popover>
+                            <PopoverTrigger asChild>
+                                <Button
+                                    variant="outline"
+                                    data-empty={!date}
+                                    name="due-date"
+                                    className="justify-start text-left font-normal data-[empty=true]:text-muted-foreground"
+                                >
+                                    <CalendarIcon />
+                                    {date ? (
+                                        format(date, "PPP")
+                                    ) : (
+                                        <span>Pick a date</span>
+                                    )}
+                                </Button>
+                            </PopoverTrigger>
+                            <PopoverContent className="w-auto p-0">
+                                <Calendar
+                                    mode="single"
+                                    selected={date}
+                                    onSelect={setDate}
+                                />
+                            </PopoverContent>
+                        </Popover>
+                    </Field>
+                </Field>
+
+                <div className="ml-auto flex gap-2">
+                    <Button variant="outline">Cancel</Button>
+                    <Button>Create task</Button>
+                </div>
+            </FieldGroup>
+        </form>
+    );
+};
