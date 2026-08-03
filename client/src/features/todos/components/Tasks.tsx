239e9380db89aaebc8e5dc83ea1d00455088253e
j-zunino <zuninojuanbautista@gmail.com>
2026-08-02 23:11:33 -0300

    feat(client): add mobile view to Tasks

--- client/src/features/todos/components/Tasks.tsx
+++ client/src/features/todos/components/Tasks.tsx
@@ -1,6 +1,12 @@
-import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
 import {
     Popover,
     PopoverContent,
@@ -12,20 +18,26 @@ import { PlusIcon } from "@phosphor-icons/react";
 import { CreateTaskForm } from "./CreateTaskForm";
 
 import type { House } from "@/features/houses/types";
+import { useMediaQuery } from "@/lib/hooks/useMediaQuery";
 
 interface Props {
     slug: House["slug"];
 }
 
 export const Tasks = ({ slug }: Props) => {
+    const isDesktop = useMediaQuery("(min-width: 768px)");
+
     return (
         <Card className="max-w-sm">
             <CardHeader>
                 <CardTitle>Tasks</CardTitle>
             </CardHeader>
             <CardContent>
-                {/* TODO: Add mobile view */}
-                <TasksPopover slug={slug} />
+                {isDesktop ? (
+                    <TasksPopover slug={slug} />
+                ) : (
+                    <TasksDialog slug={slug} />
+                )}
             </CardContent>
         </Card>
     );
@@ -48,5 +60,25 @@ export const TasksPopover = ({ slug }: Props) => {
                 <CreateTaskForm slug={slug} />
             </PopoverContent>
         </Popover>
+    );
+};
+
+export const TasksDialog = ({ slug }: Props) => {
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button variant="outline">
+                    <PlusIcon /> New task
+                </Button>
+            </DialogTrigger>
+
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Create new task</DialogTitle>
+                </DialogHeader>
+
+                <CreateTaskForm slug={slug} />
+            </DialogContent>
+        </Dialog>
     );
 };
